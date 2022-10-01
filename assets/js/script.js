132b6831952aa4c0867895b9fbb964a69b564c1a
Ed Denham <ed.denham@gmail.com>
2022-10-01 21:58:28 +0000

    Replace missing body tags

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+   const startButton =  document.getElementById('start-button')
+   console.log(startButton)
+
+})
+
+
+/**
+ * Prepares game for user by randomising order of Detectives array, 
+ * randomly assigning cards to each player and displaying a "Loading" screen
+ */
+const prepareGame = () => {
+ alert('hello world')
+} 
