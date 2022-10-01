00a186d2091e865ce8f5cc014e7eb102dd440471
Ed Denham <ed.denham@gmail.com>
2022-10-01 22:33:26 +0000

    Display loading text to user at game start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-   const startButton =  document.getElementById('start-button')
-   console.log(startButton)
+document.addEventListener("DOMContentLoaded", () => {
+    const startButton = document.getElementById('start-button')
+    startButton.addEventListener('click', prepareGame)
 
 })
+
+/** 
+ * Recursive function to display mock loading text to user 
+ * */
+const addLoadingItem = (loadingArray, i) => {
+    if (i < loadingArray.length) {
+        setTimeout(() => {
+            document.getElementById('middle-area').innerHTML = 
+            `<div>
+            ${loadingArray[i]}...
+            </div>`
+            i++
+            addLoadingItem(loadingArray, i)
+        }, 3000)
+    }
+}
 
 
 /**
@@ -10,5 +26,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
  * randomly assigning cards to each player and displaying a "Loading" screen
  */
 const prepareGame = () => {
- alert('hello world')
-} 
+    // Add Loading Text to Screen in Sequence
+    const loadingArray = ['Gathering Clues', 'Polishing Magnifying Glass', 'Sharpening Pencil', 'Interviewing Witnesses']
+    let loadingText = ''
+    console.log(loadingArray.length)
+    addLoadingItem(loadingArray, 0)
+
+}
