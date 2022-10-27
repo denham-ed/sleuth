5c39541a859ce81d382785c35cc1cc3854ecfcee
Ed Denham <ed.denham@gmail.com>
2022-10-27 21:42:16 +0000

    Create page and structure for detective listing

---
+++ assets/js/detectives-page.js
@@ -0,0 +1,18 @@
+const renderDetectives = ()=>{
+    let detectiveListing = ''
+    detectives.forEach((detective) => {
+        detectiveListing += `<div class='detective-listing'>
+        <div class="listing-left">
+            <div class=listing-image></div>
+        </div>
+        <div class="listing-middle">
+            <div>${detective.name}</div>
+            <div>Created by: Some Author</div>
+        </div>
+        <div class="listing-right">A fake biography goes here. They have all these shows and have solved loads and loads of cases</div>
+        </div>`
+    })
+    document.getElementById('detectives-listing').innerHTML = detectiveListing
+}
+
+renderDetectives()
