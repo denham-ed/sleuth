274171163f83c1808c652e8f3ae569dfe90f4627
Ed Denham <ed.denham@gmail.com>
2022-11-05 23:39:36 +0000

    Add add comments to detectives-pages.js

--- assets/js/detectives-page.js
+++ assets/js/detectives-page.js
@@ -1,21 +1,27 @@
+/**
+ * Displays home button to user
+ */
 const showButtons = () => {
     const button = document.getElementsByClassName('home-button')[0]
     button.style.display = 'block'
 }
 
-
-const renderDetectives = ()=>{
-    let detectiveListing = ''
+/**
+ * Renders a listing for each detective to the screen
+ */
+const renderDetectives = () => {
+    let detectiveListing = ""
     detectives.forEach((detective) => {
-        detectiveListing += `<div class='detective-listing'>
-        <div class="listing-left">
-            <div class=listing-image style="background-image: url(${detective.image});"></div>
-        </div>
-        <div class="listing-middle">
-            <h2>${detective.name}</h2>
-            <div><span class="red">Created by:</span> ${detective.creator}</div>
-        </div>
-        <div class="listing-right">${detective.biography}</div>
+        detectiveListing +=
+            `<div class='detective-listing'>
+            <div class="listing-left">
+                <div class=listing-image style="background-image: url(${detective.image});"></div>
+            </div>
+            <div class="listing-middle">
+                <h2>${detective.name}</h2>
+                <div><span class="red">Created by:</span> ${detective.creator}</div>
+            </div>
+            <div class="listing-right">${detective.biography}</div>
         </div>`
     })
     document.getElementById('detectives-listing').innerHTML = detectiveListing
